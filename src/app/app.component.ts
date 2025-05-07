d2eadb0c7c599957713484e3d1be628fd687f7db
Guillermo <roblesguillermolopez@gmail.com>
2025-05-07 10:10:52 +0200

    Implement create task modal component and refactor task creation layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,162 +22,33 @@ import {addIcons} from "ionicons";
 import {addOutline, todayOutline, calendarOutline, checkmarkDoneOutline, closeOutline, timeOutline, readerOutline, reorderThreeOutline} from "ionicons/icons";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
+import {CreateTaskModalComponent} from "./component/create-task-modal/create-task-modal.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [RouterModule, IonApp, ReactiveFormsModule, IonTextarea, IonCheckbox, IonSelect, IonSelectOption, IonRouterOutlet, FormsModule, CommonModule, IonInput, IonDatetime, IonItem, IonList, IonContent, IonMenu, IonRouterLink, IonIcon, IonButton, IonLabel, IonModal, IonHeader, IonToolbar, IonButtons, IonTitle, IonListHeader],
+  imports: [RouterModule, CreateTaskModalComponent, IonApp, ReactiveFormsModule, IonTextarea, IonCheckbox, IonSelect, IonSelectOption, IonRouterOutlet, FormsModule, CommonModule, IonInput, IonDatetime, IonItem, IonList, IonContent, IonMenu, IonRouterLink, IonIcon, IonButton, IonLabel, IonModal, IonHeader, IonToolbar, IonButtons, IonTitle, IonListHeader],
 })
 export class AppComponent {
   // Decorators
-  @ViewChild('createTaskModal') createTaskModal!: IonModal;
-
-  // Variables
-  createTaskForm: FormGroup;
-  selectedDate: string = '';
-  formattedDate: string = '';
-  showDatePicker: boolean = false;
-  selectedTime: string = '';
-  formattedTime: string = '';
-  showTimePicker: boolean = false;
-  allDay: boolean = false;
-  taskButtonDisabled: boolean = true;
-
-  constructor(private form: FormBuilder) {
+  @ViewChild(CreateTaskModalComponent) taskModal!: CreateTaskModalComponent;
+  constructor() {
     addIcons({ addOutline, todayOutline, calendarOutline, checkmarkDoneOutline, closeOutline, timeOutline, readerOutline, reorderThreeOutline });
-
-    this.createTaskForm = this.form.group({
-      title: ['', [Validators.required]],
-      date: ['', [Validators.required]],
-      time: [''],
-      description: [''],
-      list: ['list1'],
-    });
-
-    this.createTaskForm.valueChanges.subscribe(() => {
-      this.taskButtonDisabled = !(this.createTaskForm.get('title')?.value && this.createTaskForm.get('date')?.value);
-    });
   }
 
-  // createTaskForm: cancel button
-  cancelCreateTask(): void {
-    this.allDay = false;
-    this.formattedDate = '';
-    this.formattedTime = '';
-    this.selectedDate = '';
-    this.selectedTime = '';
-
-    this.createTaskForm.reset({
-      title: '',
-      date: '',
-      time: '',
-      description: '',
-      list: 'list1'
-    });
-
-    this.createTaskModal.dismiss(null, 'cancel');
-  }
-
-  // createTaskForm: change date
-  toggleDatePicker(): void {
-    this.showTimePicker = false;
-    this.showDatePicker = true;
-    setTimeout(() => {
-      const dateTimeElement = document.querySelector('ion-datetime');
-      if (dateTimeElement) {
-        (dateTimeElement as any).open();
+  openModal(): void {
+    this.taskModal.openModal().then((result) => {
+      if (result && result.role === 'confirm' && result.data) {
+        console.log('Tarea creada:', result.data);
       }
-    }, 100);
-  }
-
-  // createTaskForm: change time
-  toggleTimePicker(): void {
-    this.showDatePicker = false;
-    this.showTimePicker = true;
-    setTimeout(() => {
-      const dateTimeElement = document.querySelector('ion-datetime');
-      if (dateTimeElement) {
-        (dateTimeElement as any).open();
-      }
-    }, 100);
-  }
-
-  // createTaskForm: save the selected date
-  dateSelected(): void {
-    if (this.selectedDate) {
-      const date: Date = new Date(this.selectedDate);
-      this.formattedDate = date.toLocaleDateString('es-ES', {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric'
-      });
-
-      this.createTaskForm.patchValue({
-        date: this.selectedDate
-      });
-
-      this.showDatePicker = false;
-    }
-  }
-
-  // createTaskForm: save the selected time
-  timeSelected(): void {
-    if (this.selectedTime) {
-      const time = new Date(this.selectedTime);
-      this.formattedTime = time.toLocaleTimeString('es-ES', {
-        hour: '2-digit',
-        minute: '2-digit',
-      });
-
-      this.createTaskForm.patchValue({
-        time: this.selectedTime
-      });
-
-      this.showTimePicker = false;
-    }
-  }
-
-  // createTaskForm: Checkbox to set a task for the whole day
-  allDayCheck(): void {
-    this.showTimePicker = false;
-    this.allDay = !this.allDay;
-
-    if (this.allDay) {
-      this.formattedTime = '';
-      this.createTaskForm.patchValue({
-        time: ''
-      });
-    }
-  }
-
-  // createTaskForm: Check if the form is valid
-  hasErrors(controlName: string, errorName: string): boolean {
-    const control = this.createTaskForm.get(controlName);
-    return control != null;
-  }
-
-  // createTaskForm: confirm button
-  createTask(): void {
-    if(this.createTaskForm.invalid) {
-      this.createTaskForm.markAllAsTouched();
-      return;
-    }
-
-    this.formattedDate = '';
-    this.formattedTime = '';
-    this.selectedDate = '';
-    this.selectedTime = '';
-    this.allDay = false;
-
-    this.createTaskForm.reset({
-      title: '',
-      date: '',
-      time: '',
-      description: '',
-      list: 'list1'
     });
-
-    this.createTaskModal.dismiss('confirm', 'confirm');
   }
+
+  /*
+  handleTaskCreated(taskData: any): void {
+    console.log('Tarea creada mediante evento:', taskData);
+  }
+  */
+
 }
---
+++ src/app/component/create-task-modal/create-task-modal.component.ts
@@ -0,0 +1,169 @@
+import {Component, ViewChild} from '@angular/core';
+import {addIcons} from "ionicons";
+import { addOutline, todayOutline, calendarOutline, checkmarkDoneOutline, closeOutline, timeOutline, readerOutline, reorderThreeOutline } from "ionicons/icons";
+import {FormGroup, FormsModule, ReactiveFormsModule, Validators, FormBuilder} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {
+  IonApp, IonButton, IonButtons,
+  IonCheckbox, IonContent, IonDatetime, IonHeader, IonIcon,
+  IonInput, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonModal, IonPopover, IonRouterLink,
+  IonRouterOutlet,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea, IonTitle, IonToolbar
+} from "@ionic/angular/standalone";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-create-task-modal',
+  templateUrl: './create-task-modal.component.html',
+  styleUrls: ['./create-task-modal.component.scss'],
+  imports: [RouterModule, IonApp, ReactiveFormsModule, IonTextarea, IonCheckbox, IonSelect, IonSelectOption, IonRouterOutlet, FormsModule, CommonModule, IonInput, IonDatetime, IonItem, IonList, IonContent, IonMenu, IonRouterLink, IonIcon, IonButton, IonLabel, IonModal, IonHeader, IonToolbar, IonButtons, IonTitle, IonListHeader, IonPopover],
+  standalone: true
+})
+export class CreateTaskModalComponent {
+  // Decorators
+  @ViewChild('createTaskModal') createTaskModal!: IonModal;
+  //@Output() taskCreated = new EventEmitter<any>();
+
+  // Variables
+  createTaskForm: FormGroup;
+  selectedDate: string = '';
+  formattedDate: string = '';
+  showDatePicker: boolean = false;
+  selectedTime: string = '';
+  formattedTime: string = '';
+  showTimePicker: boolean = false;
+  allDay: boolean = false;
+  taskButtonDisabled: boolean = true;
+  showDatePopover: boolean = false;
+  showTimePopover: boolean = false;
+  datePopoverEvent: any;
+  timePopoverEvent: any;
+
+  constructor(private form: FormBuilder) {
+    addIcons({ addOutline, todayOutline, calendarOutline, checkmarkDoneOutline, closeOutline, timeOutline, readerOutline, reorderThreeOutline });
+
+    this.createTaskForm = this.form.group({
+      title: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      time: [''],
+      description: [''],
+      list: ['list1'],
+    });
+
+    this.createTaskForm.valueChanges.subscribe(() => {
+      this.taskButtonDisabled = !(this.createTaskForm.get('title')?.value && this.createTaskForm.get('date')?.value);
+    });
+  }
+
+  openModal() {
+    this.createTaskModal.present();
+    return this.createTaskModal.onDidDismiss();
+  }
+
+  cancelCreateTask(): void {
+    this.allDay = false;
+    this.formattedDate = '';
+    this.formattedTime = '';
+    this.selectedDate = '';
+    this.selectedTime = '';
+
+    this.createTaskForm.reset({
+      title: '',
+      date: '',
+      time: '',
+      description: '',
+      list: 'list1'
+    });
+
+    this.createTaskModal.dismiss(null, 'cancel');
+  }
+
+  // createTaskForm: change date
+  openDatePopover(ev: Event): void {
+    this.datePopoverEvent = ev;
+    this.showDatePopover = true;
+  }
+
+  openTimePopover(ev: Event): void {
+    this.timePopoverEvent = ev;
+    this.showTimePopover = true;
+  }
+
+  // createTaskForm: save the selected date
+  dateSelected(): void {
+    if (this.selectedDate) {
+      const date: Date = new Date(this.selectedDate);
+      this.formattedDate = date.toLocaleDateString('es-ES', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+      });
+
+      this.createTaskForm.patchValue({
+        date: this.selectedDate
+      });
+
+      this.showDatePicker = false;
+    }
+  }
+
+  // createTaskForm: save the selected time
+  timeSelected(): void {
+    if (this.selectedTime) {
+      const time = new Date(this.selectedTime);
+      this.formattedTime = time.toLocaleTimeString('es-ES', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+
+      this.createTaskForm.patchValue({
+        time: this.selectedTime
+      });
+
+      this.showTimePicker = false;
+    }
+  }
+
+  // createTaskForm: Checkbox to set a task for the whole day
+  allDayCheck(): void {
+    this.showTimePicker = false;
+    this.allDay = !this.allDay;
+
+    if (this.allDay) {
+      this.formattedTime = '';
+      this.createTaskForm.patchValue({
+        time: ''
+      });
+    }
+  }
+
+  // createTaskForm: confirm button
+  createTask(): void {
+    if(this.createTaskForm.invalid) {
+      this.createTaskForm.markAllAsTouched();
+      return;
+    }
+
+    const taskData = {...this.createTaskForm.value};
+
+    this.formattedDate = '';
+    this.formattedTime = '';
+    this.selectedDate = '';
+    this.selectedTime = '';
+    this.allDay = false;
+
+    this.createTaskForm.reset({
+      title: '',
+      date: '',
+      time: '',
+      description: '',
+      list: 'list1'
+    });
+
+    //this.taskCreated.emit(taskData);
+
+    this.createTaskModal.dismiss(taskData, 'confirm');
+  }
+}
